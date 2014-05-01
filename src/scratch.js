b0d1ee3902d74ddfe7d1133bb63abe2f9b35c0bd
dillonkrug <dpkrug@gmail.com>
2014-05-01 04:07:59 -0400

    Remove Testing stuff.

--- src/scratch.js
+++
@@ -1,7 +0,0 @@
-var args = ['a','b','c','d','e']
-
-// for (var args=[],l=arguments.length,i=0;i<l;args.push(arguments[i++]));
-// var l, i, times = [];
-// for (l=args.length,i=0;i<l;times.push(0), i++);
-
-console.log(args.sort(function() { return Math.random()-0.5; }));
